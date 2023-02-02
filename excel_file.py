13614b46ae41bada306ccf219412254d4cc72db2
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2023-02-02 11:37:57 +0100

    first commit made by using PyCharm update:
    figuring out how to work with Excel library and beginning to work with Airtable package.

--- excel_file.py
+++ excel_file.py
@@ -1,5 +1,30 @@
 from openpyxl import load_workbook
+import os
+
+AIRTABLE_API_KEY = os.environ["AIRTABLE_API_KEY"]
 
 workbook = load_workbook(filename="dividend_file.xlsx")
 
-print(workbook)
+sheet = workbook.active
+
+stock_shares = {}
+for row in sheet.iter_rows(min_row=7, max_row=12, min_col=2, max_col=20, values_only=True):
+    stock_id = row[1]
+    stock = {
+        "Ticker": row[1],
+        "Company Name": row[2],
+        "Sector": row[3],
+        "Industry": row[4],
+        "Headquarter": row[5]
+    }
+    stock_shares[stock_id] = stock
+
+print(stock_shares.keys())
+
+
+
+
+
+
+
+
